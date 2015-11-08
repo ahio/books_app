55a13045eb07cc1398a95ac07970c2f50651e674
Stepan Lopushansky <stepan.te.ua@gmail.com>
2015-11-08 18:02:46 +0200

    added routes to author profile page, and book's genre

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Router, Route, Link } from 'react-router';
+import React from "react"
+import ReactDOM from "react-dom"
+import { Router, Route, Link } from 'react-router'
 import { Provider } from 'react-redux'
-import hello from './main'
+import BooksList from './main'
 import Book from './book'
+import Author from './author'
+import Genre from './genre'
 import configureStore from './configStore'
 
 const store = configureStore();
@@ -11,8 +13,10 @@ const store = configureStore();
 ReactDOM.render(
     <Provider store={store}>
         <Router>
-            <Route path="/" component={hello}/>
+            <Route path="/" component={BooksList}/>
             <Route path="/book/:bookTitle" component={Book}/>
+            <Route path="/author/:authorId" component={Author}/>
+            <Route path="/genre/:genre" component={Genre}/>
         </Router>
     </Provider>,
     document.getElementById('container')
