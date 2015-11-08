199a4650bfd048c2f04c5be3fe8387e314f76747
Stepan Lopushansky <stepan.te.ua@gmail.com>
2015-11-08 03:22:55 +0200

    route for book page

--- client/src/app.js
+++ client/src/app.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, Link } from 'react-router';
 import { Provider } from 'react-redux'
-import hello from './hello'
+import hello from './main'
+import Book from './book'
 import configureStore from './configStore'
 
 const store = configureStore();
@@ -11,6 +12,7 @@ ReactDOM.render(
     <Provider store={store}>
         <Router>
             <Route path="/" component={hello}/>
+            <Route path="/book/:bookTitle" component={Book}/>
         </Router>
     </Provider>,
     document.getElementById('container')
